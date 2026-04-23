b84460a0c8264b7d8ef531fc4e19154e63a493e5
Ayush Koli <ayushkoli0709@gmail.com>
2026-04-23 11:21:54 +0530

    routes and controllers setup

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,20 +1,21 @@
 import {v2 as cloudinary} from "cloudinary"
-import { log } from "console";
 import fs from "fs"
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uplodOnCloudinary = async (localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath)=>{
     try{
         if(!localFilePath) return null
-        const respone=await cloudinary.uploader.upload(localFilePath, {
+        const response=await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("file uploaded on cloudinary", respone.url);
-        return respone;
+        console.log("file uploaded on cloudinary", response.url);
+        
+        return response;
         
     }catch(error){
         fs.unlinkSync(localFilePath)
@@ -22,4 +23,4 @@ const uplodOnCloudinary = async (localFilePath)=>{
     }
 }
 
-export {uplodOnCloudinary}
+export {uploadOnCloudinary}
